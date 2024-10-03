158f3976240ed5fac0103b54aec3bbbfab2b8e40
José Reyes Palacio <jrpalacio.dev@gmail.com>
2024-10-03 13:37:28 -0600

    feat(web): add RegimenesFiscales.json

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,30 @@
 <script setup>
 import UserList from '@/components/UserList.vue'
 const users = [
-  { id: 1, name: 'Jose Reyes Palacio', rfc: 'REPJ891214QE8', pass: '123456' },
-  { id: 2, name: 'Jose Mateo Reyes Bernal', rfc: 'DOEJ124567534', pass: '123' },
-  { id: 3, name: 'Rosa Jovanna Bernal Fuentes', rfc: 'SMITJ1234567123', pass: '123' },
-  { id: 4, name: 'Jane Smith', rfc: 'SMITJ123456789', pass: '123' },
-  { id: 5, name: 'John Doe', rfc: 'DOEJ123456789', pass: '123' },
-  { id: 6, name: 'Jane Doe', rfc: 'DOEJ123fasfasd', pass: '123' },
-  { id: 7, name: 'John Smith', rfc: 'SMasdfasdfas', pass: '123' },
-  { id: 8, name: 'Jane Doe', rfc: 'DOasvasdfas  ', pass: '123' }
+  {
+    id: 1,
+    name: 'Jose Reyes Palacio',
+    rfc: 'REPJ891214QE8',
+    pass: '123456',
+    type: 1,
+    taxSystem: '603'
+  },
+  {
+    id: 2,
+    name: 'Jose Mateo Reyes Bernal',
+    rfc: 'DOEJ124567534',
+    pass: '123',
+    type: 1,
+    taxSystem: '603'
+  },
+  {
+    id: 3,
+    name: 'Rosa Jovanna Bernal Fuentes',
+    rfc: 'SMITJ1234567123',
+    pass: '123',
+    type: 1,
+    taxSystem: '603'
+  }
 ]
 </script>
 
