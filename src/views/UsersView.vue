cdaad5ee259fb265dab69459c5faa03542b1cf28
José Reyes Palacio <jrpalacio.dev@gmail.com>
2024-10-10 11:06:30 -0600

    feat(UsersView): add tryCatch a manipulación de datos de supabase

--- src/views/UsersView.vue
+++ src/views/UsersView.vue
@@ -4,29 +4,33 @@ import UserList from '@/components/UserList.vue'
 import { supabase } from '@/supabase'
 import { onMounted, ref } from 'vue'
 
-const users = ref(null)
+const users = ref([])
 
 onMounted(async () => {
-  const { data: contribuyentes } = await supabase
-    .from('contribuyentes')
-    .select('id, nombre, apellido_paterno, apellido_materno, rfc, clave, tipo')
+  try {
+    const { data: contribuyentes } = await supabase
+      .from('contribuyentes')
+      .select('id, nombre, apellido_paterno, apellido_materno, rfc, clave, tipo')
 
-  const { data: regimen_por_contribuyente } = await supabase
-    .from('regimen_por_contribuyente')
-    .select('id_contribuyente, id_regimen')
+    const { data: regimen_por_contribuyente } = await supabase
+      .from('regimen_por_contribuyente')
+      .select('id_contribuyente, id_regimen')
 
-  users.value = contribuyentes.map((contribuyente) => {
-    return {
-      id: contribuyente.id,
-      name: `${contribuyente.nombre} ${contribuyente.apellido_paterno} ${contribuyente.apellido_materno}`,
-      rfc: contribuyente.rfc,
-      pass: contribuyente.clave,
-      type: contribuyente.tipo,
-      taxSystem: regimen_por_contribuyente.find(
-        (regimen) => regimen.id_contribuyente === contribuyente.id
-      ).id_regimen
-    }
-  })
+    users.value = contribuyentes.map((contribuyente) => {
+      return {
+        id: contribuyente.id,
+        name: `${contribuyente.nombre} ${contribuyente.apellido_paterno} ${contribuyente.apellido_materno}`,
+        rfc: contribuyente.rfc,
+        pass: contribuyente.clave,
+        type: contribuyente.tipo,
+        taxSystem: regimen_por_contribuyente.find(
+          (regimen) => regimen.id_contribuyente === contribuyente.id
+        ).id_regimen
+      }
+    })
+  } catch (error) {
+    console.error('Error al obtener los usuarios: ', error)
+  }
 })
 </script>
 
