8d86ddd2b1ac8b513ed1fa011273444fef1278bc
José Reyes Palacio <jrpalacio.dev@gmail.com>
2024-10-09 18:34:50 -0600

    feat(UsersView): add query contribuyentes

--- src/views/UsersView.vue
+++ src/views/UsersView.vue
@@ -1,31 +1,17 @@
 <script setup>
 import UserList from '@/components/UserList.vue'
-const users = [
-  {
-    id: 1,
-    name: 'Jose Reyes Palacio',
-    rfc: 'REPJ891214QE8',
-    pass: '123456',
-    type: 1,
-    taxSystem: '603'
-  },
-  {
-    id: 2,
-    name: 'Jose Mateo Reyes Bernal',
-    rfc: 'DOEJ124567534',
-    pass: '123',
-    type: 1,
-    taxSystem: '607'
-  },
-  {
-    id: 3,
-    name: 'Rosa Jovanna Bernal Fuentes',
-    rfc: 'SMITJ1234567123',
-    pass: '123',
-    type: 1,
-    taxSystem: '621'
-  }
-]
+
+import { supabase } from '@/supabase'
+import { onMounted, ref } from 'vue'
+
+const users = ref(null)
+
+onMounted(async () => {
+  const { data: contribuyentes } = await supabase
+    .from('contribuyentes')
+    .select('id, nombre, apellido_paterno, apellido_materno, rfc, clave, tipo')
+  users.value = contribuyentes
+})
 </script>
 
 <template>
