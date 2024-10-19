b82783931924ccb23cae7fe674cdea7925d64ae1
José Reyes Palacio <jrpalacio.dev@gmail.com>
2024-10-18 18:06:05 -0600

    feat(web): add autentificación en app

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <script setup>
+import { supabase } from '@/supabase'
 import NavBar from './components/NavBar.vue'
 import { RouterView } from 'vue-router'
+import router from './router'
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  supabase.auth.onAuthStateChange((event, session) => {
+    if (!session) {
+      router.push({ name: 'login' })
+    }
+  })
+})
 </script>
 
 <template>
   <NavBar />
-  <main class="main">
-    <RouterView />
-  </main>
+  <main class="main"><RouterView /></main>
 </template>
 
 <style scoped>
