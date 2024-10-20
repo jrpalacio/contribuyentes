fa16eda441a4b016105106dc75dd718bcf8242f2
José Reyes Palacio <jrpalacio.dev@gmail.com>
2024-10-19 18:40:33 -0600

    feat(UsersView): add controlador de ventana de nuevo registro

--- src/views/UsersView.vue
+++ src/views/UsersView.vue
@@ -1,16 +1,22 @@
 <script setup>
+import FormNew from '@/components/FormNew.vue'
 import UserList from '@/components/UserList.vue'
+import IconUserPlus from '@/icons/IconUserPlus.vue'
 
 import { supabase } from '@/supabase'
 import { onMounted, ref } from 'vue'
+import { useTaxplayerStore } from '@/stores/taxpayer'
 
 const users = ref([])
+
+const contrib = useTaxplayerStore()
+const { setShowFormNewContribuyente } = contrib
 
 onMounted(async () => {
   try {
     const { data: contribuyentes } = await supabase
       .from('contribuyentes')
-      .select('id, nombre, apellido_paterno, apellido_materno, rfc, clave, tipo')
+      .select('id, nombre, apellido_paterno, apellido_materno, rfc, clave, tipo, regimenes')
 
     const { data: regimen_por_contribuyente } = await supabase
       .from('regimen_por_contribuyente')
@@ -32,8 +38,38 @@ onMounted(async () => {
     console.error('Error al obtener los usuarios: ', error)
   }
 })
+
+function handleBtnShowFormNew() {
+  setShowFormNewContribuyente(true)
+}
 </script>
 
 <template>
-  <UserList :users />
+  <UserList :users>
+    <template #button>
+      <button class="btn--add-user" @click="handleBtnShowFormNew">
+        <IconUserPlus />
+      </button>
+    </template>
+  </UserList>
+  <FormNew />
 </template>
+
+<style scoped>
+.btn--add-user {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 48px;
+  width: 48px;
+  border: 0;
+  background-color: coral;
+  color: #fff;
+  border-radius: 0.9rem;
+  cursor: pointer;
+
+  &:hover {
+    background-color: rgb(253, 93, 34);
+  }
+}
+</style>
