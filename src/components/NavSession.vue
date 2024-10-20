d01dcd3ff266957e06f1d6fb5ccbc2bc6a0eefb4
José Reyes Palacio <jrpalacio.dev@gmail.com>
2024-10-19 18:39:54 -0600

    feat(NavSession): add NavSession component

---
+++ src/components/NavSession.vue
@@ -0,0 +1,24 @@
+<script setup>
+import IconLogin from '@/icons/IconLogin.vue'
+</script>
+<template>
+  <nav class="nav--session">
+    <h3>Contadora Bernal</h3>
+    <RouterLink to="/login"><IconLogin /><span>Inicia sesión</span></RouterLink>
+  </nav>
+</template>
+
+<style scoped>
+a {
+  text-decoration: none;
+  display: flex;
+  flex-direction: row;
+  gap: 0.5rem;
+}
+.nav--session {
+  display: flex;
+  justify-content: space-between;
+  padding: 0.75rem 1rem;
+  background-color: #242424;
+}
+</style>
