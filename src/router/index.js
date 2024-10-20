a625caf199a9674a3aba55e68b95dcaf1575473b
José Reyes Palacio <jrpalacio.dev@gmail.com>
2024-10-20 14:15:14 -0600

    fix(router): fix estructura del router

--- src/router/index.js
+++ src/router/index.js
@@ -3,42 +3,40 @@ import UsersView from '../views/UsersView.vue'
 import UserDetailView from '@/views/UserDetailView.vue'
 import LoginView from '@/views/LoginView.vue'
 
-const routes = [
-  {
-    path: '/',
-    name: 'users',
-    component: UsersView,
-    meta: { requiresAuth: true }
-  },
-  {
-    path: '/login',
-    name: 'login',
-    component: LoginView
-  },
-  {
-    path: '/users/:id',
-    name: 'user-detail',
-    component: UserDetailView,
-    meta: { requiresAuth: true }
-  },
-  {
-    path: '/about',
-    name: 'about',
-
-    // route level code-splitting
-    // this generates a separate chunk (About.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import('../views/AboutView.vue')
-  }
-]
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes
+  routes: [
+    {
+      path: '/users',
+      name: 'users',
+      component: UsersView,
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView
+    },
+    {
+      path: '/users/:id',
+      name: 'user-detail',
+      component: UserDetailView,
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/about',
+      name: 'about',
+
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
+    }
+  ]
 })
 
 router.beforeEach((to, from, next) => {
   const isLoggedIn = !!localStorage.getItem('supabase.auth.token')
-  console.log(to.matched)
   if (to.matched.some((record) => record.meta.requiresAuth) && !isLoggedIn) {
     next({ name: 'users' })
   } else {
