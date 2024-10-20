4cba832f16d22b06b61ebbdbc84771b6cafa5de2
José Reyes Palacio <jrpalacio.dev@gmail.com>
2024-10-20 16:21:25 -0600

    feat(web): no permite el acceso a ninguna pagina si no esa logueado

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,15 @@ import { createRouter, createWebHistory } from 'vue-router'
 import UsersView from '../views/UsersView.vue'
 import UserDetailView from '@/views/UserDetailView.vue'
 import LoginView from '@/views/LoginView.vue'
+import { supabase } from '@/supabase'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/',
+      redirect: '/users'
+    },
     {
       path: '/users',
       name: 'users',
@@ -35,13 +40,28 @@ const router = createRouter({
   ]
 })
 
-router.beforeEach((to, from, next) => {
-  const isLoggedIn = !!localStorage.getItem('supabase.auth.token')
+/* router.beforeEach(async (to, from, next) => {
+  const { data: response } = await supabase.auth.getUser()
+  if (error) throw error 
+
+const isLoggedIn = !!response.user.id
+  console.log(response.user.id)
   if (to.matched.some((record) => record.meta.requiresAuth) && !isLoggedIn) {
     next({ name: 'users' })
   } else {
     next()
-  }
+  } 
+  
+}) */
+
+router.beforeEach(async (to, from, next) => {
+  const { data: response, error } = await supabase.auth.getUser()
+  let isAuthenticated = null
+  if (error) isAuthenticated = false
+  else isAuthenticated = !!response.user.id
+
+  if (to.name !== 'login' && !isAuthenticated) next({ name: 'login' })
+  else next()
 })
 
 export default router
