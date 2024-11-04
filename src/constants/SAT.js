ee63077567b456de97f1b569ecf194b31f65943c
José Reyes Palacio <jrpalacio.dev@gmail.com>
2024-11-03 21:20:24 -0600

    feat(SAT): add MESSAGE

--- src/constants/SAT.js
+++ src/constants/SAT.js
@@ -7,3 +7,10 @@ export const LISTA_TIPO_DE_CONTRIBUYENTE = [
   { id: 1, descripcion: 'Persona Física' },
   { id: 2, descripcion: 'Persona Moral' }
 ]
+
+export const MESSAGE = {
+  SELECCIONAR_REGIMEN: 'Seleccione un régimen fiscal',
+  SELECCIONAR_TIPO: 'Seleccione un tipo de contribuyente',
+  PERSONA_FISICA: 'Persona Física',
+  PERSONA_MORAL: 'Persona Moral'
+}
