500acc06448cf10cb1e8e4de851ec3d831d02ac6
José Reyes Palacio <jrpalacio.dev@gmail.com>
2024-11-06 13:33:46 -0600

    style(app): add background

--- src/App.vue
+++ src/App.vue
@@ -47,6 +47,21 @@ watchEffect(async () => {
   align-items: center;
   height: 100%;
   padding: 0.8rem;
+  background-image: linear-gradient(
+    to right top,
+    #d16ba5,
+    #c777b9,
+    #ba83ca,
+    #aa8fd8,
+    #9a9ae1,
+    #8aa7ec,
+    #79b3f4,
+    #69bff8,
+    #52cffe,
+    #41dfff,
+    #46eefa,
+    #5ffbf1
+  );
 }
 .container {
   display: grid;
