8a0dbe82e2c71d907ead460f0e26a3bbf8a3eb61
José Reyes Palacio <jrpalacio.dev@gmail.com>
2024-11-06 16:28:22 -0600

    style(app): modificar estilos de inicio de sesión

--- src/App.vue
+++ src/App.vue
@@ -47,22 +47,20 @@ watchEffect(async () => {
   align-items: center;
   height: 100%;
   padding: 0.8rem;
-  background-image: linear-gradient(
-    to right top,
-    #d16ba5,
-    #c777b9,
-    #ba83ca,
-    #aa8fd8,
-    #9a9ae1,
-    #8aa7ec,
-    #79b3f4,
-    #69bff8,
-    #52cffe,
-    #41dfff,
-    #46eefa,
-    #5ffbf1
-  );
+  background-size: 100% 100%;
+  background-position:
+    0px 0px,
+    0px 0px,
+    0px 0px,
+    0px 0px,
+    0px 0px;
+  background-image: repeating-linear-gradient(315deg, #00ffff2e 92%, #073aff00 100%),
+    repeating-radial-gradient(75% 75% at 238% 218%, #00ffff12 30%, #073aff14 39%),
+    radial-gradient(99% 99% at 109% 2%, #00c9ffff 0%, #073aff00 100%),
+    radial-gradient(99% 99% at 21% 78%, #7b00ffff 0%, #073aff00 100%),
+    radial-gradient(160% 154% at 711px -303px, #2000ffff 0%, #073affff 100%);
 }
+
 .container {
   display: grid;
   grid-template-columns: 1fr;
